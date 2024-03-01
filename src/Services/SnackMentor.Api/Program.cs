7f8e6708ced154c1f1eaa03dc8b3e7fcdfb436ad
Michał Gronko <michalgronko@gmail.com>
2024-03-01 19:51:28 +0100

    Initial Commit

---
+++ src/Services/SnackMentor.Api/Program.cs
@@ -0,0 +1,15 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
