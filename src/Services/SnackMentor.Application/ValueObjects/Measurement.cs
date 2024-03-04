50f9974db3bf0db432c8184a1f2ea4fd389bcd5f
Michał Gronko <michalgronko@gmail.com>
2024-03-04 20:10:17 +0100

    Added methods for converting from grams to other units

--- src/Services/SnackMentor.Application/ValueObjects/Measurement.cs
+++ src/Services/SnackMentor.Application/ValueObjects/Measurement.cs
@@ -4,23 +4,30 @@ namespace SnackMentor.Application.ValueObjects;
 
 public sealed record Measurement
 {
-    public decimal AmountInGrams { get; }
-    public UnitType Unit { get; }
+    public decimal Amount { get; }
+    public UnitType Unit => UnitType.Grams;
 
     public Measurement(decimal amount, UnitType unit)
     {
-        AmountInGrams = ConvertToGrams(amount, unit);
-
-        Unit = unit;
+        Amount = Save(amount, unit);
     }
 
-    private decimal ConvertToGrams(decimal amount, UnitType unit)
-    {
-        return unit switch
+    private decimal Save(decimal amount, UnitType unit) 
+        => unit switch
         {
             UnitType.Grams => amount,
             UnitType.Ounces => amount / 0.03527396M,
-            _ => AmountInGrams
+            _ => Amount
         };
-    }
+
+    public decimal DisplayAs(UnitType unit) 
+        => unit switch
+        {
+            UnitType.Grams => Amount,
+            UnitType.Ounces => Amount / 28.3495m,
+            UnitType.Cups => Amount / 236.588m,
+            UnitType.Teaspoons => Amount / 5.91m,
+            UnitType.Tablespoons => Amount / 14.79m,
+            _ => throw new ArgumentOutOfRangeException(nameof(unit), unit, null)
+        };
 }
