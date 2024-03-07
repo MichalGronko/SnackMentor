48d368e3b973c522e0668545f7a48740fe541f12
Michał Gronko <michalgronko@gmail.com>
2024-03-07 22:08:43 +0100

    Minor refactoring

--- src/Services/SnackMentor.Application/Services/UnitsConverterService.cs
+++ src/Services/SnackMentor.Application/Services/UnitsConverterService.cs
@@ -2,33 +2,21 @@
 
 public sealed class UnitsConverterService
 {
-    public double ConvertGramsToOunces(double grams)
-    {
-        return grams * 0.03527396;
-    }
+    public double ConvertGramsToOunces(double grams) 
+        => grams * 0.03527396;
 
-    public double ConvertOuncesToGrams(double ounces)
-    {
-        return ounces / 0.03527396;
-    }
+    public double ConvertOuncesToGrams(double ounces) 
+        => ounces / 0.03527396;
 
-    public double ConvertCupsToGrams(double cups)
-    {
-        return cups * 236.588;
-    }
+    public double ConvertCupsToGrams(double cups) 
+        => cups * 236.588;
 
-    public double ConvertGramsToCups(double grams)
-    {
-        return grams / 236.588;
-    }
+    public double ConvertGramsToCups(double grams) 
+        => grams / 236.588;
 
-    public double ConvertTeaspoonsToTablespoons(double teaspoons)
-    {
-        return teaspoons / 3;
-    }
+    public double ConvertTeaspoonsToTablespoons(double teaspoons) 
+        => teaspoons / 3;
 
-    public double ConvertTablespoonsToTeaspoons(double tablespoons)
-    {
-        return tablespoons * 3;
-    }
+    public double ConvertTablespoonsToTeaspoons(double tablespoons) 
+        => tablespoons * 3;
 }
