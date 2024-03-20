4d5ef240a5ccd72405a3905b377437e3953930b4
Michał Gronko <michalgronko@gmail.com>
2024-03-20 23:23:05 +0100

    Added Category.cs

---
+++ src/Services/SnackMentor.Application/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace SnackMentor.Application.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int ParentId { get; set; }
+    public string Path { get; set; }
+}
