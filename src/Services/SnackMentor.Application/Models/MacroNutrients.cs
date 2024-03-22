0bf4316b47501caccd2d03888759aaf7e954379c
Michał Gronko <michalgronko@gmail.com>
2024-03-22 22:46:50 +0100

    Added more factories to MacroNutrients.cs and cleaned up a bit

--- src/Services/SnackMentor.Application/Models/MacroNutrients.cs
+++ src/Services/SnackMentor.Application/Models/MacroNutrients.cs
@@ -4,30 +4,32 @@ namespace SnackMentor.Application.Models;
 
 public sealed class MacroNutrients
 {
-    // public Measurement Fat { get; set; }
-    // public Measurement Carbohydrates { get; set; }
-    // public Measurement Protein { get; set; }
-    // public Measurement Fiber { get; set; }
-    
     public Mass Fat { get; private set; }
     public Mass Carbohydrates { get; private set; }
     public Mass Protein { get; private set; }
-
-    public void Create(Mass fat, Mass carbohydrates, Mass protein, NutrientStrategy strategy)
-    {
-        if (strategy == NutrientStrategy.Per100Gram)
+    
+    public static MacroNutrients CreateFromMass(Mass fat, Mass carbs, Mass protein) 
+        => new()
         {
-            Fat = fat;
-            Carbohydrates = carbohydrates;
-            Protein = protein;
-        }
-        
-        // if(strategy == )
-        
-        
+            Fat = fat,
+            Carbohydrates = carbs,
+            Protein = protein
+        };
+
+    public static MacroNutrients CreateFromPercentMass(double fatPercent, double carbsPercent, double proteinPercent)
+    {
+        Mass mass = Mass.FromGrams(100);
+
+        return new MacroNutrients
+        {
+            Fat = mass * fatPercent,
+            Carbohydrates = mass * carbsPercent,
+            Protein = mass * proteinPercent
+        };
     }
 
-    public static MacroNutrients CreateNew(Density fatDensity, Density carbsDensity, Density proteinDensity, Density solutionDensity)
+    public static MacroNutrients CreateFromDensity(Density fatDensity, Density carbsDensity, Density proteinDensity,
+        Density solutionDensity)
     {
         Mass mass = Mass.FromGrams(100);
 
@@ -38,39 +40,4 @@ public sealed class MacroNutrients
             Protein = mass * (proteinDensity / solutionDensity)
         };
     }
-    
-    public void Create(Mass fat, Mass carbohydrates, Mass protein, Density substanceDensity)
-    {
-        // Volume volume = Volume.FromMilliliters(100);
-        //
-        // Mass fat = Mass.FromGrams(fatInGrams) / volume;
-        // Mass carbs = Mass.FromGrams(carbsInGrams) / volume;
-        // Mass protein = Mass.FromGrams(proteinInGrams) / volume;
-        //
-        // // Calculate mass per 100g using density
-        // Mass fatPer100g = fat * density;
-        // Mass carbsPer100g = carbs * density;
-        // Mass proteinPer100g = protein * density;
-        //
-        // return (fatPer100g, carbsPer100g, proteinPer100g);
-
-        // Density x = fat / Volume.FromMilliliters(100);
-        // var y = x.Value * fat.Value
-        // Fat = (fat / Volume.FromMilliliters(100)) * density
-    }
-    
-    
-    // public static double ConvertTeaspoonsToGrams(double teaspoons)
-    // {
-    //     Volume volumeInTeaspoons = Volume.FromMetricTeaspoons(teaspoons);
-    //     var massInGrams = volumeInTeaspoons.ToUnit();
-    //
-    //     return massInGrams.Grams;
-    // }
-}
-
-public enum NutrientStrategy
-{
-    Per100Gram,
-    Per100Milliliters
 }
